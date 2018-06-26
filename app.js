e4076940245e4eedec2af4c816081929b45c8dcb
Paul Forsyth <paul.forsyth@ecs-digital.co.uk>
2018-06-26 22:17:02 +0100

    change message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello again World!'))
 module.exports = app
