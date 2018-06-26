503e4196be0c36d0d52226d116de269de5fae795
Paul Forsyth <paul.forsyth@ecs-digital.co.uk>
2018-06-26 22:32:20 +0100

    revert to hello world

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('<html><b>Hello World!</b></html>'))
+app.get('/', (req, res) => res.send('Hello World!'))
 module.exports = app
